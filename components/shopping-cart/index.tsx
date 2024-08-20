9d8702bca5a75b7d35b67f9a18fa158933d0e601
BTC415 <alenschmitt415@gmail.com>
2024-08-19 19:25:06 -0700

    Modified shopping cart general  page

--- components/shopping-cart/index.tsx
+++ components/shopping-cart/index.tsx
@@ -4,11 +4,11 @@ import Item from './item';
 import { RootState } from 'store';
 
 const ShoppingCart = () => {
-  const { cartItems } = useSelector((state: RootState)  => state.cart);
+  const { cartItems } = useSelector((state: RootState) => state.cart);
 
   const priceTotal = () => {
     let totalPrice = 0;
-    if(cartItems.length > 0) {
+    if (cartItems.length > 0) {
       cartItems.map(item => totalPrice += item.price * item.count);
     }
 
@@ -28,7 +28,7 @@ const ShoppingCart = () => {
             <table>
               <tbody>
                 <tr>
-                  <th style={{textAlign: 'left'}}>Product</th>
+                  <th style={{ textAlign: 'left' }}>Product</th>
                   <th>Color</th>
                   <th>Size</th>
                   <th>Ammount</th>
@@ -37,7 +37,7 @@ const ShoppingCart = () => {
                 </tr>
 
                 {cartItems.map(item => (
-                  <Item 
+                  <Item
                     key={item.id}
                     id={item.id}
                     thumb={item.thumb}
@@ -49,18 +49,17 @@ const ShoppingCart = () => {
                   />
                 ))}
               </tbody>
-            </table> 
-          } 
-          
-          {cartItems.length === 0 && 
+            </table>
+          }
+
+          {cartItems.length === 0 &&
             <p>Nothing in the cart</p>
           }
         </div>
-      
+
         <div className="cart-actions">
           <a href="/products" className="cart__btn-back"><i className="icon-left"></i> Continue Shopping</a>
           <input type="text" placeholder="Promo Code" className="cart__promo-code" />
-
           <div className="cart-actions__items-wrapper">
             <p className="cart-actions__total">Total cost <strong>${priceTotal().toFixed(2)}</strong></p>
             <a href="/cart/checkout" className="btn btn--rounded btn--yellow">Checkout</a>
@@ -71,5 +70,4 @@ const ShoppingCart = () => {
   )
 };
 
-  
 export default ShoppingCart
