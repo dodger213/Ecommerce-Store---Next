39e98581739c091555fec6f4da544ef63f751d32
BTC415 <alenschmitt415@gmail.com>
2024-08-20 07:04:19 -0700

    Modified the style of subscribe page

--- components/subscribe/index.tsx
+++ components/subscribe/index.tsx
@@ -2,7 +2,7 @@ const Subscribe = () => {
   return (
     <section className="subscribe">
       <div className="container">
-        <div style={{backgroundImage: 'url(/images/subscribe.jpg)'}} className="subscribe__content">
+        <div style={{ backgroundImage: 'url(/images/subscribe.jpg)' }} className="subscribe__content">
           <h4>Subscribe to our newsletter and receive exclusive offers every week</h4>
 
           <form className="subscribe__form">
