63f7067584c721a1b7714c86083bafbe3cdc6857
BTC415 <alenschmitt415@gmail.com>
2024-08-21 07:59:33 -0700

    Modified 404 component

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,7 +4,7 @@ const ErrorPage = () => (
   <LayoutError>
     <section className="error-page">
       <div className="container">
-        <h1>Error 404</h1>
+        <h1>Error 404!</h1>
         <p>Woops. Looks like this page doesn't exist</p>
         <a href="#" className="btn btn--rounded btn--yellow">Go to home</a>
       </div>
